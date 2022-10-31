14ea512803cd754e9dcd2575a6c8c4e8c6ca7cd1
michellebessa <michelleandressa12@gmail.com>
2022-10-31 16:36:38 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,120 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TodoListReactFetch = () => {
+  
+  const [list, setList] = useState([]);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+  const [inputValue, setInputValue] = useState("");
+
+  const backendURL =
+    "https://assets.breatheco.de/apis/fake/todos/user/michellebessa";
+
+  const getList = () => {
+    fetch(backendURL)
+      .then((res) => res.json())
+      .then((data) => setList(data));
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  const addItemsList = (inputValue) => {
+    let newTodoArray = [
+      ...list,
+      {
+        label: inputValue,
+        done: false,
+      },
+    ];
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/michellebessa", {
+      method: "PUT",
+      body: JSON.stringify(newTodoArray),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (!res.ok) throw Error(res.statusText);
+        return res.json();
+      })
+      .then((response) => {
+        console.log("Success:", response);
+        getList();
+      })
+      .catch((error) => console.error(error));
+    setInputValue("");
+  };
+
+  function removeItem(i) {
+  let filtered = list.filter((task, index)=>{
+    i != index;
+    return i != index;
+  })
+  fetch(
+    "https://assets.breatheco.de/apis/fake/todos/user/michellebessa",
+    {
+      method: "PUT",
+      body: JSON.stringify(filtered),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((res) => {
+      if (!res.ok) throw Error(res.statusText);
+      return res.json();
+    })
+    .then((response) => {
+      console.log("Success:", response);
+      getList();
+    })
+    .catch((error) => console.error(error));
+  setTodoArray(filtered);
+  }
+
+  let mappedTodoArray = list.map((task, index) => {
+		return (
+			<li>
+				{task.label}
+				<button onClick={() => removeItem(index)}>
+        <i class="fas fa-trash"></i>
+				</button>{" "}
+			</li>
+		);
+	});
+
+const clearItem=()=>{
+ setList([])
+}
+
+  return (
+		<div className="todoBox">
+			<div className="header">
+				<h1>Todos</h1>
+				<button className="clearAll" onClick={() => clearItem()}>
+					{" "}
+					Clear All{" "}
+				</button>
+			</div>
+			<div className="inputField">
+				<input
+					id="newTask"
+          onChange={(e)=>setInputValue(e.target.value)}
+					type="text"
+					placeholder="Type something to do..."
+					onKeyUp={(e) => {
+						if (e.key === "Enter") {
+							addItemsList(inputValue);
+						}
+					}}
+				/>
+			</div>
+			<div>
+				<ul>{mappedTodoArray}</ul>
+			</div>
 		</div>
 	);
 };
 
-export default Home;
+export default TodoListReactFetch;
